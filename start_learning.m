04cd72e9369c455b5bffc9b4bc1c89db7289a516
熱田亮 <atsutaryou@atsutaryous-MacBook-puro.local>
2019-03-21 15:11:54 +0900

    Add: F-Score実装

--- start_learning.m
+++ start_learning.m
@@ -24,21 +24,42 @@ plot(iteration_list, j_list);
 
 correct = 0;
 incorrect = 0;
+true_positive_count = 0;
+false_positive_count = 0;
+true_negative_count = 0;
+false_negative_count = 0;
 
 for i = 1:m
   positive = hypothesis(-10 + (Theta' * csv.training_data(:, i))) >= 0.5;
 
-  if positive == csv.training_labels(i)
+  if positive == 1 && positive == csv.training_labels(i)
     correct++;
-  else
+    true_positive_count++;
+  elseif positive == 1 && positive != csv.training_labels(i)
     incorrect++;
+    false_positive_count++;
+  elseif positive == 0 && positive == csv.training_labels(i)
+    correct++;
+    true_negative_count++;
+  elseif positive == 0 && positive != csv.training_labels(i)
+    incorrect++;
+    false_negative_count++;
   end
 end
 
 accuracy = (correct / (correct + incorrect)) * 100;
+precision = true_positive_count / (true_positive_count + false_positive_count + 0.0000001);
+recall = true_positive_count / (true_positive_count + false_negative_count + 0.0000001);
+fscore = 2 * (precision * recall) / (precision + recall + 0.0000001);
 
 disp("Accuracy: ");
 disp(accuracy);
+disp("Precision: ");
+disp(precision);
+disp("Recall: ");
+disp(recall);
+disp("F-Score: ");
+disp(fscore);
 
 while 1
   input_list = [];
