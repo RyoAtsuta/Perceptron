f1db11207c25a8faa5afe4379510a735849a9a34
熱田亮 <atsutaryou@atsutaryous-MacBook-puro.local>
2019-03-22 18:02:26 +0900

    Add: and_generator.mをさらに改善。精度100%に上昇

--- lib/Generator/and_generator.m
+++ lib/Generator/and_generator.m
@@ -3,13 +3,20 @@ function data = generator(feature_count)
   add_number = 1;
   data.training_data = [];
 
-  rand_val = rand(1);
+  rand_val1 = rand(1);
+  rand_val2 = rand(1);
 
-  if rand_val > 0.5
+  if rand_val1 > 0.5
     data.training_data = round(random_initialize(feature_count, 1, range)) > 0;
   else
-    for i = 1:feature_count
-      data.training_data = [data.training_data; add_number];
+    if rand_val2 > 0.5
+      for i = 1:feature_count
+        data.training_data = [data.training_data; add_number];
+      end
+    else
+      rand_index = round(rand(1) * (feature_count-1)) + 1;
+      data.training_data = ones(feature_count, 1);
+      data.training_data(rand_index) = 0;
     end
   end
 
